2ea39b25a7460072b2c0cfefe00922d7c58d1f22
shango <shannon.gold@gmail.com>
2026-04-01 22:54:46 -0600

    Bump version to v13-beta-7, version-tagged LOG_PREFIX in all Script Editor messages

--- ExportGenie.py
+++ ExportGenie.py
@@ -51,7 +51,7 @@ from maya.OpenMayaUI import MQtUtil
 # Constants
 # ---------------------------------------------------------------------------
 TOOL_NAME = "ExportGenie"
-TOOL_VERSION = "v13-beta-6"
+TOOL_VERSION = "v13-beta-7"
 WINDOW_NAME = "multiExportWindow"
 WORKSPACE_CONTROL_NAME = "exportGenieWorkspaceControl"
 SHELF_BUTTON_LABEL = "ExportGenie"
